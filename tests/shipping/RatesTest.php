ad740384107fbbdd8fbfa7beab1f7f3719654a41
lu <luis709394@gmail.com>
2020-05-27 15:53:23 -0400

    Adds travis test.

---
+++ tests/shipping/RatesTest.php
@@ -0,0 +1,22 @@
+<?php
+use  FlagshipWoocommerce\Requests\Rates_Request;
+
+class RatesTest extends FlagshipShippingUnitTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->package = require __DIR__.'/../fixture/Package.php';
+        $this->zoneSettings = require __DIR__.'/../fixture/ZoneSettings.php';
+    }
+
+    public function testCheckoutRates()
+    {
+    	$rateRequest = new Rates_Request($this->zoneSettings['token']);
+    	$rateRequest->setApiUrl('https://test-api.smartship.io');
+    	$rates = $rateRequest->getRates($this->package, $this->zoneSettings);
+
+    	$this->assertGreaterThan(count($rates), 0);        
+    }
+}
